84beeb265414ad2d99e32738d8d1d77209b0d1aa
sbdream8 <91125316+sbdream8@users.noreply.github.com>
2024-11-10 20:28:11 -0800

    Add Hyperlink

--- my-portfolio/app/components/Contact.js
+++ my-portfolio/app/components/Contact.js
@@ -1,12 +1,18 @@
 // app/components/Contact.js
+import Link from 'next/link';
+
 export default function Contact() {
   return (
     <div>
       <h2>Contact Information</h2>
-        <p>Email: sebindream@gmail.com</p>
-        <p>LinkedIn: https://www.linkedin.com/in/sebin-lee-4758542b0/</p>
-        <p>Phone: +1-323-974-4446</p>
-      {/* Add other contact details here */}
+      <p>Email: sebindream@gmail.com</p>
+      <p>Phone: +1-323-974-4446</p>
+      <p>
+        LinkedIn:{" "}
+        <Link href="https://www.linkedin.com/in/sebin-lee-4758542b0/" target="_blank">
+          https://www.linkedin.com/in/sebin-lee-4758542b0/
+        </Link>
+      </p>
     </div>
   );
 }
