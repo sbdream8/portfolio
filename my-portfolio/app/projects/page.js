c3ba535b38e2a48ca0c7f92f41f4eb4c9b821c00
sbdream8 <91125316+sbdream8@users.noreply.github.com>
2024-11-12 11:47:26 -0800

    My Projects->Projects

--- my-portfolio/app/projects/page.js
+++ my-portfolio/app/projects/page.js
@@ -4,7 +4,7 @@ import Projects from '../components/Projects'; // Adjusted to use correct path
 export default function ProjectsPage() {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>My Projects</h1>
+      <h1>Projects</h1>
       <Projects />
     </div>
   );
